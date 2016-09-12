e69442eaafa640c75ac197056a7262e521601d3a
Humbal Shahi <sh.humble@gmail.com>
2016-09-12 10:27:51 -0700

    created custom module hello

---
+++ modules/custom/hello/src/Controller/HelloController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\commissions_and_hearings\Controller\HelloController.
+ */
+
+namespace Drupal\hello\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class HelloController extends ControllerBase {
+  public function content() {
+    return array(
+        '#type' => 'markup',
+        '#markup' => $this->t('Hello, World!'),
+    );
+  }
+}
---
+++ modules/custom/hello/src/Plugin/Block/HelloBlock.php
@@ -0,0 +1,23 @@
+<?php
+namespace Drupal\hello\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hello' Block
+ *
+ * @Block(
+ *   id = "hello_block",
+ *   admin_label = @Translation("Hello block"),
+ * )
+ */
+class HelloBlock extends BlockBase {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function build () {
+		return array (
+			'#markup' => $this->t('Hello, world!')
+		);
+	}
+}
